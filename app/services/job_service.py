ffd7b8eb81f02a3565b4529e35472baf0f34047f
Maria Elena Paredes <ma.elena.pg@gmail.com>
2026-07-14 17:09:32 -0500

    Simplify Greenhouse job response

--- app/services/job_service.py
+++ app/services/job_service.py
@@ -1,5 +1,21 @@
 from app.services.greenhouse_client import get_greenhouse_jobs
 
+
 def get_jobs():
     data = get_greenhouse_jobs()
-    return data["jobs"][:10]
+    jobs = []
+
+    for job in data["jobs"][:10]:
+        jobs.append({
+            "id": job["id"],
+            "title": job["title"],
+            "company_name": job.get("company_name", "Stripe"),
+            "location": job["location"]["name"],
+            "absolute_url": job["absolute_url"],
+            "first_published": job.get("first_published"),
+            "updated_at": job.get("updated_at"),
+            "language": job.get("language"),
+            "application_deadline": job.get("application_deadline")
+        })
+
+    return jobs
